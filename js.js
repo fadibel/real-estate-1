8f3360f9594d98d9c75ab7e3d6c958f48a510c6c
belkacemi fadi <158364049+fadibel@users.noreply.github.com>
2024-03-25 02:26:55 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,6 @@
+function togglemenu() {
+    const togglemenu = document.querySelector(".togglemenu")
+    const navigation = document.querySelector(".navigation")
+    togglemenu.classList.toggle("active")
+    navigation.classList.toggle("active")
+}
